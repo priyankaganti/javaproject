a4bbacdc8cd07583b1d1b0394dd0f94ba9f58151
Priyanka Ganti <varsha.ganti@gmail.com>
2017-01-24 16:24:05 +0530

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -3,7 +3,15 @@ public class ArraySum
 {
 public static void main(String args[])
 {
-int a[]={5,10,15,4};
+int n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+int a[]=new int[n];
+System.out.println("Enter all elements:");
+for(int i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
 int sum=0;
 for(int i=0;i<=3;i++)
 {
